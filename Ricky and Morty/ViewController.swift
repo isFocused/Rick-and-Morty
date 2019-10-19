938aad40cdd3e2712c74426a8df225b32c59c939
Denis <ignorid@gmai.com>
2019-10-19 20:57:05 +0300

    Initial Commit

---
+++ Ricky and Morty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ricky and Morty
+//
+//  Created by Денис Иванов on 19.10.2019.
+//  Copyright © 2019 Денис Иванов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
